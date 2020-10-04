3215b1514ae84fc903b94f792c134191dfc72b50
Sergey Gorovov <gor-serg@bk.ru>
2020-10-04 22:53:43 +0300

    added finished state

--- src/App.jsx
+++ src/App.jsx
@@ -7,21 +7,40 @@ import Question from './Question/Question'
 function App() {
   const [ questions ] = React.useState(questionsBase())
   const [ currentQuestion, setCurrentQuestion] = React.useState(0)
+  const [ score, setScore ] = React.useState(0)
+  const [ finished, setFinished ] = React.useState(false)
 
   function nextQuestion() {
     if (currentQuestion < questions.length - 1) {
       setCurrentQuestion(currentQuestion + 1)
+    } else {
+      setFinished(true)
     }
+  }
+
+  let FinishScreen
+  if (finished) {
+    FinishScreen = <p>Result: {score}/{questions.length}</p>
+  }
+
+  let Questions
+  if (!finished) {
+    Questions = <>
+      <h3>Question #{currentQuestion + 1}</h3>
+      {questions.map((q, index) => (
+        <Question data={q} visible={index === currentQuestion} addScore={() => setScore(score + 1)}/>
+      ))}
+      <button onClick={nextQuestion}>
+        {currentQuestion == (questions.length - 1)  ? 'Finish' : 'Next'}
+      </button>
+    </>
   }
 
   return (
     <div className={styles.App}>
       <h1>English test</h1>
-      <h3>Question #{currentQuestion + 1}</h3>
-      {questions.map((q, index) => (
-        <Question data={q} visible={index === currentQuestion}/>
-      ))}
-      <button onClick={nextQuestion}>Next</button>
+      { Questions }
+      { FinishScreen }
     </div>
   );
 }
--- src/Question/Question.jsx
+++ src/Question/Question.jsx
@@ -2,25 +2,34 @@ import React from 'react'
 
 import styles from './Question.module.css'
 
-function Question({data, visible}) {
+function Question({data, visible, addScore}) {
   const [ answered, setAnswered ] = React.useState(false)
 
-
+  function handleClick(answer) {
+    setAnswered(answer)
+    if (answer.correct) {
+      addScore()
+    }
+  }
 
   return (
     <div className={styles.Question} style={{display: !visible && 'none'}}>
       <p>{data.question}</p>
-      <div style={{display: 'flex', gap: '0.5rem'}}>
+      <div style={{display: 'flex', gap: '0.5rem', flexWrap: 'wrap'}}>
         {data.answers.map(a=>(
           <button
             key={a.answer}
             disabled={answered}
             onClick={() => 
               {
-                setAnswered(a)
+                handleClick(a)
               }
             }
-            className={(answered && a.correct) ? styles.Question_correct : null}
+            className={[
+              styles.Question_Button,
+              answered.answer === a.answer ? styles.Question_Button_picked : null,
+              answered && a.correct ? styles.Question_Button_correct : null
+            ].join(' ')}
           >
             {a.answer}
           </button>
